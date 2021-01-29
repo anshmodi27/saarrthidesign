be498aa16248684b1bef56b13a518d02e0a6bc65
Ansh Modi <74901469+anshmodi27@users.noreply.github.com>
2021-01-29 22:47:16 +0530

    Update contact.php

--- contact.php
+++ contact.php
@@ -51,6 +51,8 @@
     $headers .= "CC: hasmukhmodi1012@gmail.com\r\n";
     $headers .= "MIME-Version: 1.0\r\n";
     $headers .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
+    
+    mail($to, $subject, $message, $headers);
 
     if ($_SERVER['REQUEST_METHOD'] == 'POST') {
         $name = $_POST['name'];
